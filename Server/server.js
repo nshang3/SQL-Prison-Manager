085249fb7b514a91a54788bfa443581156bf7bba
Utkarsh <ukohli4@uwo.ca>
2024-12-01 20:50:59 -0500

    Fixed formatting of form and removed redundant call in server.js

--- Server/server.js
+++ Server/server.js
@@ -54,7 +54,6 @@ app.use('/api/cells', cellRoute);
 app.use('/api/guards', guardRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/incidents', incidentRoutes);
-app.use('/api/cells', cellRoute);
 
 // Route to serve the homepage (index.html)
 app.get('/', (req, res) => {
